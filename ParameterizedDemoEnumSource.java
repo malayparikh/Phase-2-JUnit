95b930e3dc9e94412abc469d8a6ef61f673bedf3
Malay Parikh <63942863+malayparikh@users.noreply.github.com>
2023-11-22 10:57:20 +0530

    Create ParameterizedDemoEnumSource.java

---
+++ ParameterizedDemoEnumSource.java
@@ -0,0 +1,21 @@
+package com.app.junitDemo;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class ParameterizedDemoEnumSource {
+
+	enum Values {
+
+		Selenium, Jmeter, Junit, TestNG
+
+	}
+
+	@ParameterizedTest(name = "Enum value {arguments}")
+	@EnumSource(Values.class)
+	public void passvaluesEnumSource(Object value) {
+
+		System.out.println(value.toString());
+	}
+
+}
